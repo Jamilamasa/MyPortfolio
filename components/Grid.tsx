a9a84a480b6916234b0b66642d7a698fb88e7ab2
JamilAmasa <jamilamasa@gmail.com>
2024-07-12 11:50:53 +0100

    feat(about): add about section

---
+++ components/Grid.tsx
@@ -0,0 +1,31 @@
+import { gridItems } from "@/data";
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+type Props = {};
+
+const Grid = (props: Props) => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map(
+          ({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassname={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { FaLocationArrow } from "react-icons/fa6";
 
 type Props = {};
 
@@ -26,7 +29,10 @@ const Hero = (props: Props) => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Jamil Amasa's Portfolio</h2>
+            <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless User Experiences"/>
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Jamil, a Software Engineer.</p>
+            <a href="#about"><MagicButton title="Show my work" icon={<FaLocationArrow />} position="right"/></a>
         </div>
       </div>
     </div>
