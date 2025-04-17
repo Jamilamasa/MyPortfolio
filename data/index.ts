fb444aae673dfdccce251dc4d9ad509d74efff48
Jamil <95282243+Jamilamasa@users.noreply.github.com>
2025-04-17 16:39:32 +0100

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "FullStack Software Engineer",
+    title: "Frontend Software Developer",
     description: "I am a",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building an online ticketing web application",
+    title: "Currently building projects with Next.js. I'm also expanding my backend skills by learning FastAPI.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
